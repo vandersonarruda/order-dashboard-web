cdc517aa62dc5e090acb5fc14c0b04b74aa92720
Vanderson Arruda <contato@vanderson.com.br>
2024-02-03 12:29:59 -0300

    chore: config eslint lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
-
   return (
-    <h1>Hello World</h1>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Click here</Button>
+    </div>
   )
 }
