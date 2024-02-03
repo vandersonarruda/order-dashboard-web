650bbfc65361085fa50182332a2f3392d8600064
Vanderson Arruda <contato@vanderson.com.br>
2024-02-03 13:08:50 -0300

    chore: config Helmet lib

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,10 @@
+import { Helmet } from 'react-helmet-async'
 import { Outlet } from 'react-router-dom'
 
 export function AppLayout() {
   return (
     <div>
+      <Helmet title="Dashboard" />
       <h1>Header APP</h1>
 
       <div>
